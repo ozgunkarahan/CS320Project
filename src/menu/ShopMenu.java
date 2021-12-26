fef32ced57eec07318d5a8ec65dc576ff60ee446
merttan99 <mert.tan@ozu.edu.tr>
2021-12-26 22:08:39 +0300

    Update ShopMenu class

--- src/menu/ShopMenu.java
+++ src/menu/ShopMenu.java
@@ -1,59 +1,73 @@
-package menu;
-
-import java.util.Scanner;
-
-public class ShopMenu {
+	package menu;
 	
-	public static void Shop() {
+	import java.util.Scanner;
+	
+	public class ShopMenu {
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("1 - Sword piece: 10$");
-		System.out.println("2 - Armor piece: 10$"); 
-		System.out.println("3 - Shield piece: 10$"); 
-		System.out.println("4 - Staff piece: 10$");
-		System.out.println("Pick an inventory to buy: ");
-		int choice = scanner.nextInt();
+		public static void Shop() {
+			
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("1 - Sword piece: 10$");
+			System.out.println("2 - Armor piece: 10$"); 
+			System.out.println("3 - Shield piece: 10$"); 
+			System.out.println("4 - Staff piece: 10$");
+			System.out.println("Pick an inventory to buy: ");
+			int choice = scanner.nextInt();
+			boolean run = true;
+	        /*while (run) {
+	            switch (choice) {
+	                case 1 -> {
+	                    if (player.currency >= 10) {
+	                        inventory.add(inventory[0]);
+	                        player.currency = player.currency - 10;
+	
+	                    } else {
+	                        System.out.println("You don't have enough currency.");
+	                    }
+	                    run = false;
+	                }
+	                case 2 -> {
+	                    if (player.currency >= 10) {
+	                        inventory.add(inventory[1]);
+	                        player.currency = player.currency - 10;
+	
+	                    } else {
+	                        System.out.println("You don't have enough currency.");
+	                    }
+	                    run = false;
+	                }
+	                case 3 -> {
+	                    if (choice == 3) {
+	                        if (player.currency >= 10) {
+	                            inventory.add(inventory[2]);
+	                            player.currency = player.currency - 10;
+	
+	                        } else {
+	                            System.out.println("You don't have enough currency.");
+	                        }
+	                    }
+	                    run = false;
+	                }
+	                case 4 -> {
+	                    if(player.currency >= 10) {
+	                        inventory.add(inventory[3]);
+	                        player.currency = player.currency - 10;
+	
+	                    }else {
+	                        System.out.println("You don't have enough currency.");
+	                    }
+	                    run = false;
+	                }
+	                default -> {
+	                    System.out.println("Pick a valid number.");
+	                }
+	            }
+	        }*/
+			}
 		
-		/*if (choice == 1 ) {
-			if(player.currency >= 10) {
-				inventory.add(inventory[0]);
-				player.currency = player.currency - 10;
-				
-			}else {
-				System.out.println("You don't have enough currency.");
-			}
-		if (choice == 2 ) {
-			if(player.currency >= 10) {
-			    inventory.add(inventory[1]);
-			    player.currency = player.currency - 10;
-					
-			}else {
-				System.out.println("You don't have enough currency.");
-			}
-		}
-		if (choice == 3 ) {
-			if(player.currency >= 10) {
-				inventory.add(inventory[2]);
-				player.currency = player.currency - 10;
-					
-			}else {
-				System.out.println("You don't have enough currency.");
-			}
-		}
-		if (choice == 4 ) {
-			if(player.currency >= 10) {
-				inventory.add(inventory[3]);
-				player.currency = player.currency - 10;
-					
-			}else {
-				System.out.println("You don't have enough currency.");
-			}
-		}
-		}else {
-			System.out.println("Pick a valid number.");
-		}*/
+	
 		
 	}
 
-}
+
 		
