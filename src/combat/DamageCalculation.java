1e4cda867b1a0dd36b2d1ae1fbd53c1937634515
ozgunkarahan <ozgun.karahan@ozu.edu.tr>
2021-12-28 14:58:24 +0300

    fixed random init

--- src/combat/DamageCalculation.java
+++ src/combat/DamageCalculation.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class DamageCalculation {
 
-	private static Random rand;
+	private static Random rand = new Random();
 
 	public static double diceRoll(String name) {
 
